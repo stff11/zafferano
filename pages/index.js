e28de77456d5c4eed5b6913b1b5cacbd6106cd61
StefanoFoffanoVS <stefano.foffano@visualsoft.co.uk>
2024-08-27 14:26:15 +0200

    Add logging for debugging main

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,17 @@
 import Hero from '../components/Hero';
 
 export default function Home() {
+  console.log("Rendering Home component");
   return (
     <div>
       <h1>Zafferano di Sardegna DOP</h1>
+      {console.log("About to render Hero")}
       <Hero
-                imageSrc="/images/banner2.webp"
-                altText="Purissimo Zafferano di Sardegna"
-                captionText="Pure Saffron"
-            />
+        imageSrc="/images/banner2.webp"
+        altText="Purissimo Zafferano di Sardegna"
+        captionText="Pure Saffron"
+      />
+      {console.log("Hero rendered")}
     </div>
   )
 }
